2f4dfb2c584c84f74e24e3dd7b71d15ccff4c47a
Matt Williams <brityt@gmail.com>
2018-12-29 09:24:27 +0000

    Initial push

---
+++ index.js
@@ -0,0 +1,23 @@
+const botConfig = require('./bot-config.json')
+const Discord = require('discord.js')
+const getJSON = require('get-json')
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async() => {
+
+    console.log(`${bot.user.username} is online`);
+
+    setInterval(()=>{
+        getJSON("http://5.196.93.109:28236/status.json", (err, response) => {
+            bot.user.setActivity(response.players + "/100 online");
+        });
+    }, 100);
+});
+
+bot.on("message", (msg) => {
+    
+  
+});
+
+bot.login(botConfig.token);
